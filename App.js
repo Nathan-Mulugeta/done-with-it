b17a7d282537e1804f2fe544942744328811cd48
nathanMulugeta01 <nathanmulugetacoding@gmail.com>
2023-06-14 15:12:37 +0300

    progress

--- App.js
+++ App.js
@@ -5,40 +5,22 @@ import {
   StatusBar,
   View,
 } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppText from "./app/components/AppText";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "#fff",
-          flex: 1,
-          flexDirection: "row",
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "dodgerblue",
-            width: 100,
-            height: 100,
-          }}
-        />
-        <View
-          style={{
-            backgroundColor: "gold",
-            width: 100,
-            height: 100,
-          }}
-        />
-        <View
-          style={{
-            backgroundColor: "tomato",
-            width: 100,
-            height: 100,
-          }}
-        />
-      </View>
-    </SafeAreaView>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppText>Hello</AppText>
+    </View>
   );
 }
 
